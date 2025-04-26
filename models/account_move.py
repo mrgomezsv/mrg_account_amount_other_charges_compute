358a42083b600c0722aaf05597461135464bdd8d
Mario Roberto <mrgomez.dev@gmail.com>
2025-04-25 19:43:59 -0600

    Agrege totalizacion a losc campos cuando se agrupen

--- models/account_move.py
+++ models/account_move.py
@@ -7,18 +7,21 @@ class AccountMove(models.Model):
     mr_amount_total = fields.Monetary(
         string='Importe Adeudado',
         compute='_compute_mr_cume',
+        store=True,
         help='Muestra solo el valor del Importe Adeudado'
     )
 
     mr_other_charges = fields.Monetary(
         string='Otros Cargos',
         compute='_compute_mr_cume',
+        store=True,
         help='Muestra solo el valor de Otros Cargos'
     )
 
     mr_total_with_other_charges = fields.Monetary(
         string='Total Mas Otros Cargos',
         compute='_compute_mr_cume',
+        store=True,
         help='Muestra el valor del Total Mas Otros Cargos'
     )
 
